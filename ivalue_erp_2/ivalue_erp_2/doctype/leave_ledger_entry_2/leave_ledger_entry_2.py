1aea7d59f271e88cb79a117094c7a8e3de507eba
yazananaya0 <yazananaya0@gmail.com>
2026-04-02 11:42:59 +0300

    Second Commit (change ref doctype and remove unecessary doctypes from app)

--- ivalue_erp_2/ivalue_erp_2/doctype/leave_ledger_entry_2/leave_ledger_entry_2.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, yazan and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LeaveLedgerEntry2(Document):
-	pass
--- ivalue_erp_2/ivalue_erp_2/doctype/leave_ledger_summary/test_leave_ledger_summary.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, yazan and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLeaveLedgerSummary(FrappeTestCase):
-	pass
